8bb5da755ef46bda7610ccfd9626e89eb2188ac4
msa <mdsohailahmed117@gmail.com>
2024-08-20 06:03:23 +0400

    start

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,11 @@ app.get("/",(req,res)=>{
     res.send("Server Start Now")
 })
 
+app.get("/api/product",(req,res)=>{
+    const product=[{id:1,name:"pro1"},{id:2,name:"pro2"},{id:3,name:"pro3"}]
+    res.send(product)
+})
+
 app.listen(port,()=>{
     console.log(`server run on port  ${port}`)
 })
